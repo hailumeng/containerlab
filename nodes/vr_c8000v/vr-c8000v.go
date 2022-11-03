160517c0d142a76d770a39eb098b69a0f24b8620
Hailu Meng <hailumeng@gmail.com>
2022-11-03 19:29:26 +0000

    Adding c8000v,c9800v,cat9kv support in containerlab code base

---
+++ nodes/vr_c8000v/vr-c8000v.go
@@ -0,0 +1,152 @@
+// Copyright 2020 Nokia
+// Licensed under the BSD 3-Clause License.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package vr_c8000v
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/srl-labs/containerlab/netconf"
+	"github.com/srl-labs/containerlab/nodes"
+	"github.com/srl-labs/containerlab/runtime"
+	"github.com/srl-labs/containerlab/types"
+	"github.com/srl-labs/containerlab/utils"
+)
+
+var kindnames = []string{"vr-c8000v", "vr-cisco_c8000v"}
+
+const (
+	scrapliPlatformName = "cisco_iosxe"
+
+	configDirName   = "config"
+	startupCfgFName = "startup-config.cfg"
+
+	defaultUser     = "admin"
+	defaultPassword = "admin"
+)
+
+func init() {
+	nodes.Register(kindnames, func() nodes.Node {
+		return new(vrc8000v)
+	})
+	err := nodes.SetDefaultCredentials(kindnames, defaultUser, defaultPassword)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+type vrc8000v struct {
+	cfg     *types.NodeConfig
+	mgmt    *types.MgmtNet
+	runtime runtime.ContainerRuntime
+}
+
+func (s *vrc8000v) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
+	s.cfg = cfg
+	for _, o := range opts {
+		o(s)
+	}
+	// env vars are used to set launch.py arguments in vrnetlab container
+	defEnv := map[string]string{
+		"CONNECTION_MODE":    nodes.VrDefConnMode,
+		"USERNAME":           "admin",
+		"PASSWORD":           "admin",
+		"DOCKER_NET_V4_ADDR": s.mgmt.IPv4Subnet,
+		"DOCKER_NET_V6_ADDR": s.mgmt.IPv6Subnet,
+	}
+	s.cfg.Env = utils.MergeStringMaps(defEnv, s.cfg.Env)
+
+	// mount config dir to support startup-config functionality
+	s.cfg.Binds = append(s.cfg.Binds, fmt.Sprint(path.Join(s.cfg.LabDir, configDirName), ":/config"))
+
+	if s.cfg.Env["CONNECTION_MODE"] == "macvtap" {
+		// mount dev dir to enable macvtap
+		s.cfg.Binds = append(s.cfg.Binds, "/dev:/dev")
+	}
+
+	s.cfg.Cmd = fmt.Sprintf("--username %s --password %s --hostname %s --connection-mode %s --trace",
+		s.cfg.Env["USERNAME"], s.cfg.Env["PASSWORD"], s.cfg.ShortName, s.cfg.Env["CONNECTION_MODE"])
+
+	// set virtualization requirement
+	s.cfg.HostRequirements.VirtRequired = true
+
+	return nil
+}
+
+func (s *vrc8000v) Config() *types.NodeConfig { return s.cfg }
+
+func (s *vrc8000v) PreDeploy(_, _, _ string) error {
+	utils.CreateDirectory(s.cfg.LabDir, 0777)
+	return loadStartupConfigFile(s.cfg)
+}
+
+func (s *vrc8000v) Deploy(ctx context.Context) error {
+	cID, err := s.runtime.CreateContainer(ctx, s.cfg)
+	if err != nil {
+		return err
+	}
+	_, err = s.runtime.StartContainer(ctx, cID, s.cfg)
+	return err
+}
+
+func (*vrc8000v) PostDeploy(_ context.Context, _ map[string]nodes.Node) error {
+	return nil
+}
+
+func (s *vrc8000v) GetImages() map[string]string {
+	return map[string]string{
+		nodes.ImageKey: s.cfg.Image,
+	}
+}
+
+func (*vrc8000v) Destroy(_ context.Context) error          { return nil }
+func (s *vrc8000v) WithMgmtNet(mgmt *types.MgmtNet)        { s.mgmt = mgmt }
+func (s *vrc8000v) WithRuntime(r runtime.ContainerRuntime) { s.runtime = r }
+func (s *vrc8000v) GetRuntime() runtime.ContainerRuntime   { return s.runtime }
+
+func (s *vrc8000v) Delete(ctx context.Context) error {
+	return s.runtime.DeleteContainer(ctx, s.cfg.LongName)
+}
+
+func (s *vrc8000v) SaveConfig(_ context.Context) error {
+	err := netconf.SaveConfig(s.cfg.LongName,
+		defaultUser,
+		defaultPassword,
+		scrapliPlatformName,
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("saved %s running configuration to startup configuration file\n", s.cfg.ShortName)
+	return nil
+}
+
+func loadStartupConfigFile(node *types.NodeConfig) error {
+	// create config directory that will be bind mounted to vrnetlab container at / path
+	utils.CreateDirectory(path.Join(node.LabDir, configDirName), 0777)
+
+	if node.StartupConfig != "" {
+		// dstCfg is a path to a file on the clab host that will have rendered configuration
+		dstCfg := filepath.Join(node.LabDir, configDirName, startupCfgFName)
+
+		c, err := os.ReadFile(node.StartupConfig)
+		if err != nil {
+			return err
+		}
+
+		cfgTemplate := string(c)
+
+		err = node.GenerateConfig(dstCfg, cfgTemplate)
+		if err != nil {
+			log.Errorf("node=%s, failed to generate config: %v", node.ShortName, err)
+		}
+	}
+	return nil
+}
